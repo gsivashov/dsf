3978754d3a8b44f3371319b87799147d526666f7
gsivashov <gsivashov@gmail.com>
2020-12-03 20:15:04 +0100

    added logging and argparse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import logging
+import argparse
+
+from pathlib import Path
 from client import RestClient
 from dotenv import load_dotenv
 
@@ -13,42 +17,107 @@ def get_results(keyword):
     )
     response = client.post("/v3/serp/google/organic/live/advanced", post_data)
     if response["status_code"] == 20000:
-        return response
+        if response['tasks'][0]['status_code'] == 20000:
+            return response
+        else: logging.info("error. Code: %d Message: %s" % (response['tasks'][0]['status_code'], response['tasks'][0]["status_message"]))
     else:
-        print("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
+        logging.info("error. Code: %d Message: %s" % (response["status_code"], response["status_message"]))
         
 
-def print_result(response, keyword, fileds):
+def parse_result(response, keyword, fileds, domains):
     # parse result and return only organic result
     data = response['tasks'][0]['result'][0]['items']
     
     result = []
 
     for item in data:
-        item_list = [str(item[field]) for field in fields if item['type'] == 'organic']
+        item_list = [
+            str(item[field]) for domain in domains for field in fields 
+            if item['type'] == 'organic' and domain in item['domain']
+        ]
         item_list.insert(0,keyword)
         if len(item_list) > 1:
             result.append(item_list)
     return result
 
 
-def main(start_file, fields):
+def main(params, fields):
+
+    keywords = Path(params.keywords)
+    domains = Path(params.domains)
+    # table header:
     print(f"keyword|{'|'.join(fields)}")
-    with open(start_file) as keywords:
+
+    # main loop for sending requests:
+    with open(keywords) as keywords:
         for keyword in keywords:
             keyword = keyword.strip().lower()
-            result = get_results(keyword)
-            for row in print_result(result, keyword, fields):
-                if 'anibis.ch' in row[1]:
-                    print('|'.join(row))
+
+            logging.info(keyword)
+
+            try:
+                result = get_results(keyword)
+                for row in parse_result(result, keyword, fields, domains):
+                    print('|'.join(row), flush=True)
+
+            except Exception as e:
+                error = str(e)
+            if error:
+                logging.error(error)
+            else:
+                logging.info(
+                    f'{keyword} data SUCCESSFULLY collected'
+                )
 
 if __name__ == '__main__':
+
+    logging.basicConfig(
+        filename=Path().cwd() / 'logs' / 'main.log',
+        filemode='a',
+        level=logging.INFO,
+        format='%(levelname)s:%(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
     load_dotenv()
     login = os.getenv("LOGIN")
     password = os.getenv("PASSWORD")
 
     client = RestClient(login, password)
-    start_file = 'keywords.txt'
-    fields = ['domain', 'extended_snippet', 'faq', 'is_featured_snippet', 'rank_absolute', 'rank_group', 'type', 'url']
+    # start_file = 'keywords.txt'
 
-    main(start_file, fields)
+    fields = [
+        'domain',
+        'extended_snippet',
+        'faq',
+        'is_featured_snippet',
+        'rank_absolute',
+        'rank_group',
+        'type',
+        'url'
+    ]
+    # domains = ['www.anibis.ch']
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--domains',
+        type=str,
+        default='domains.txt',
+        help='enter path to file with DOMAINS. default: domains.txt'
+    )
+
+    parser.add_argument(
+        '--keywords',
+        type=str,
+        default='keywords.txt',
+        help='enter path to file wih KEYWORDS. default: keywords.txt'
+    )
+    
+    params = parser.parse_args()
+
+    logging.info('Started')
+    
+    main(params, fields)
+
+    logging.info('Done')
